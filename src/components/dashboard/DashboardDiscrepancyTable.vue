a36aa5a868957496654764f54737edc1b2e27c05
yangf <yangf@yangfdeMacBook-Air.local>
2026-03-31 02:26:15 +0800

    refactor: refine dashboard discrepancy table guidance

--- src/components/dashboard/DashboardDiscrepancyTable.vue
+++ src/components/dashboard/DashboardDiscrepancyTable.vue
@@ -23,18 +23,18 @@
           <a-table-summary-row class="total-row">
             <a-table-summary-cell :index="0" class="first-cell">合计</a-table-summary-cell>
             
-            <a-table-summary-cell :index="1" class="center-col highlight-cell"><span class="metric-slot metric-slot--count"><span :class="['metric-slot__value', getValueClass('deptHasCount', discrepancyTotalRow.deptHasCount), 'drilldown-value']" title="点击查看明细" @click="goToEisAudit">{{ formatDisplayValue('deptHasCount', discrepancyTotalRow.deptHasCount) }}</span></span></a-table-summary-cell>
-            <a-table-summary-cell :index="2" class="center-col highlight-cell"><span class="metric-slot metric-slot--amount"><span :class="['metric-slot__value', getValueClass('deptHasAmt', discrepancyTotalRow.deptHasAmt), 'drilldown-value']" title="点击查看明细" @click="goToEisAudit">{{ formatDisplayValue('deptHasAmt', discrepancyTotalRow.deptHasAmt) }}</span></span></a-table-summary-cell>
+            <a-table-summary-cell :index="1" class="center-col highlight-cell is-zebra-column"><span class="metric-slot metric-slot--count"><span :class="['metric-slot__value', getValueClass('deptHasCount', discrepancyTotalRow.deptHasCount), 'drilldown-value']" title="点击查看明细" @click="goToEisAudit">{{ formatDisplayValue('deptHasCount', discrepancyTotalRow.deptHasCount) }}</span></span></a-table-summary-cell>
+            <a-table-summary-cell :index="2" class="center-col highlight-cell is-zebra-column"><span class="metric-slot metric-slot--amount"><span :class="['metric-slot__value', getValueClass('deptHasAmt', discrepancyTotalRow.deptHasAmt), 'drilldown-value']" title="点击查看明细" @click="goToEisAudit">{{ formatDisplayValue('deptHasAmt', discrepancyTotalRow.deptHasAmt) }}</span></span></a-table-summary-cell>
             
             <a-table-summary-cell :index="3" class="center-col highlight-cell"><span class="metric-slot metric-slot--count"><span :class="['metric-slot__value', getValueClass('eisHasCount', discrepancyTotalRow.eisHasCount), 'drilldown-value']" title="点击查看明细" @click="goToEisAudit">{{ formatDisplayValue('eisHasCount', discrepancyTotalRow.eisHasCount) }}</span></span></a-table-summary-cell>
             <a-table-summary-cell :index="4" class="center-col highlight-cell"><span class="metric-slot metric-slot--amount"><span :class="['metric-slot__value', getValueClass('eisHasAmt', discrepancyTotalRow.eisHasAmt), 'drilldown-value']" title="点击查看明细" @click="goToEisAudit">{{ formatDisplayValue('eisHasAmt', discrepancyTotalRow.eisHasAmt) }}</span></span></a-table-summary-cell>
             
-            <a-table-summary-cell :index="5" class="center-col highlight-cell"><span class="metric-slot metric-slot--count"><span :class="['metric-slot__value', getValueClass('diffRevCount', discrepancyTotalRow.diffRevCount), 'drilldown-value']" title="点击查看明细" @click="goToEisAudit">{{ formatDisplayValue('diffRevCount', discrepancyTotalRow.diffRevCount) }}</span></span></a-table-summary-cell>
-            <a-table-summary-cell :index="6" class="center-col highlight-cell"><span class="metric-slot metric-slot--amount"><span :class="['metric-slot__value', getValueClass('diffRevAmt', discrepancyTotalRow.diffRevAmt), 'drilldown-value']" title="点击查看明细" @click="goToEisAudit">{{ formatDisplayValue('diffRevAmt', discrepancyTotalRow.diffRevAmt) }}</span></span></a-table-summary-cell>
+            <a-table-summary-cell :index="5" class="center-col highlight-cell is-zebra-column"><span class="metric-slot metric-slot--count"><span :class="['metric-slot__value', getValueClass('diffRevCount', discrepancyTotalRow.diffRevCount), 'drilldown-value']" title="点击查看明细" @click="goToEisAudit">{{ formatDisplayValue('diffRevCount', discrepancyTotalRow.diffRevCount) }}</span></span></a-table-summary-cell>
+            <a-table-summary-cell :index="6" class="center-col highlight-cell is-zebra-column"><span class="metric-slot metric-slot--amount"><span :class="['metric-slot__value', getValueClass('diffRevAmt', discrepancyTotalRow.diffRevAmt), 'drilldown-value']" title="点击查看明细" @click="goToEisAudit">{{ formatDisplayValue('diffRevAmt', discrepancyTotalRow.diffRevAmt) }}</span></span></a-table-summary-cell>
             <a-table-summary-cell :index="7" class="center-col highlight-cell"><span class="metric-slot metric-slot--count"><span :class="['metric-slot__value', getValueClass('diffProcCount', discrepancyTotalRow.diffProcCount), 'drilldown-value']" title="点击查看明细" @click="goToEisAudit">{{ formatDisplayValue('diffProcCount', discrepancyTotalRow.diffProcCount) }}</span></span></a-table-summary-cell>
             <a-table-summary-cell :index="8" class="center-col highlight-cell"><span class="metric-slot metric-slot--amount"><span :class="['metric-slot__value', getValueClass('diffProcAmt', discrepancyTotalRow.diffProcAmt), 'drilldown-value']" title="点击查看明细" @click="goToEisAudit">{{ formatDisplayValue('diffProcAmt', discrepancyTotalRow.diffProcAmt) }}</span></span></a-table-summary-cell>
-            <a-table-summary-cell :index="9" class="center-col highlight-cell"><span class="metric-slot metric-slot--count"><span :class="['metric-slot__value', getValueClass('diffGrossCount', discrepancyTotalRow.diffGrossCount), 'drilldown-value']" title="点击查看明细" @click="goToEisAudit">{{ formatDisplayValue('diffGrossCount', discrepancyTotalRow.diffGrossCount) }}</span></span></a-table-summary-cell>
-            <a-table-summary-cell :index="10" class="center-col highlight-cell"><span class="metric-slot metric-slot--amount"><span :class="['metric-slot__value', getValueClass('diffGrossAmt', discrepancyTotalRow.diffGrossAmt), 'drilldown-value']" title="点击查看明细" @click="goToEisAudit">{{ formatDisplayValue('diffGrossAmt', discrepancyTotalRow.diffGrossAmt) }}</span></span></a-table-summary-cell>
+            <a-table-summary-cell :index="9" class="center-col highlight-cell is-zebra-column"><span class="metric-slot metric-slot--count"><span :class="['metric-slot__value', getValueClass('diffGrossCount', discrepancyTotalRow.diffGrossCount), 'drilldown-value']" title="点击查看明细" @click="goToEisAudit">{{ formatDisplayValue('diffGrossCount', discrepancyTotalRow.diffGrossCount) }}</span></span></a-table-summary-cell>
+            <a-table-summary-cell :index="10" class="center-col highlight-cell is-zebra-column"><span class="metric-slot metric-slot--amount"><span :class="['metric-slot__value', getValueClass('diffGrossAmt', discrepancyTotalRow.diffGrossAmt), 'drilldown-value']" title="点击查看明细" @click="goToEisAudit">{{ formatDisplayValue('diffGrossAmt', discrepancyTotalRow.diffGrossAmt) }}</span></span></a-table-summary-cell>
           </a-table-summary-row>
         </a-table-summary>
       </template>
@@ -113,6 +113,14 @@ const renderClickableValue = (key, val) => {
   );
 };
 
+const appendHeaderClass = (baseClass, extraClass) => ({
+  class: [baseClass, extraClass].filter(Boolean).join(' ')
+});
+
+const appendCellClass = (extraClass) => ({
+  class: extraClass
+});
+
 const columns = computed(() => {
   return [
     { 
@@ -135,8 +143,8 @@ const columns = computed(() => {
           key: 'dept-has-bridge',
           customHeaderCell: () => ({ class: 'header-bridge-cell is-sub-group', rowSpan: 0 }),
           children: [
-            { title: '个数', dataIndex: 'deptHasCount', key: 'deptHasCount', align: 'center', width: 80, customHeaderCell: () => ({ class: 'is-leaf-metric' }), customRender: ({ text }) => renderClickableValue('deptHasCount', text) },
-            { title: '金额', dataIndex: 'deptHasAmt', key: 'deptHasAmt', align: 'center', width: 80, customHeaderCell: () => ({ class: 'is-leaf-metric' }), customRender: ({ text }) => renderClickableValue('deptHasAmt', text) }
+            { title: '个数', dataIndex: 'deptHasCount', key: 'deptHasCount', align: 'center', width: 80, customHeaderCell: () => appendHeaderClass('is-leaf-metric', ''), customCell: () => appendCellClass('is-zebra-column'), customRender: ({ text }) => renderClickableValue('deptHasCount', text) },
+            { title: '金额', dataIndex: 'deptHasAmt', key: 'deptHasAmt', align: 'center', width: 80, customHeaderCell: () => appendHeaderClass('is-leaf-metric', ''), customCell: () => appendCellClass('is-zebra-column'), customRender: ({ text }) => renderClickableValue('deptHasAmt', text) }
           ]
         }
       ]
@@ -166,8 +174,8 @@ const columns = computed(() => {
           title: '收入', 
           customHeaderCell: () => ({ class: 'subgroup-header-cell is-sub-group' }),
           children: [
-            { title: '个数', dataIndex: 'diffRevCount', key: 'diffRevCount', align: 'center', width: 80, customHeaderCell: () => ({ class: 'is-leaf-metric' }), customRender: ({ text }) => renderClickableValue('diffRevCount', text) },
-            { title: '金额', dataIndex: 'diffRevAmt', key: 'diffRevAmt', align: 'center', width: 80, customHeaderCell: () => ({ class: 'is-leaf-metric' }), customRender: ({ text }) => renderClickableValue('diffRevAmt', text) }
+            { title: '个数', dataIndex: 'diffRevCount', key: 'diffRevCount', align: 'center', width: 80, customHeaderCell: () => appendHeaderClass('is-leaf-metric', ''), customCell: () => appendCellClass('is-zebra-column'), customRender: ({ text }) => renderClickableValue('diffRevCount', text) },
+            { title: '金额', dataIndex: 'diffRevAmt', key: 'diffRevAmt', align: 'center', width: 80, customHeaderCell: () => appendHeaderClass('is-leaf-metric', ''), customCell: () => appendCellClass('is-zebra-column'), customRender: ({ text }) => renderClickableValue('diffRevAmt', text) }
           ]
         },
         { 
@@ -182,8 +190,8 @@ const columns = computed(() => {
           title: '毛利', 
           customHeaderCell: () => ({ class: 'subgroup-header-cell is-sub-group' }),
           children: [
-            { title: '个数', dataIndex: 'diffGrossCount', key: 'diffGrossCount', align: 'center', width: 80, customHeaderCell: () => ({ class: 'is-leaf-metric' }), customRender: ({ text }) => renderClickableValue('diffGrossCount', text) },
-            { title: '金额', dataIndex: 'diffGrossAmt', key: 'diffGrossAmt', align: 'center', width: 80, customHeaderCell: () => ({ class: 'is-leaf-metric' }), customRender: ({ text }) => renderClickableValue('diffGrossAmt', text) }
+            { title: '个数', dataIndex: 'diffGrossCount', key: 'diffGrossCount', align: 'center', width: 80, customHeaderCell: () => appendHeaderClass('is-leaf-metric', ''), customCell: () => appendCellClass('is-zebra-column'), customRender: ({ text }) => renderClickableValue('diffGrossCount', text) },
+            { title: '金额', dataIndex: 'diffGrossAmt', key: 'diffGrossAmt', align: 'center', width: 80, customHeaderCell: () => appendHeaderClass('is-leaf-metric', ''), customCell: () => appendCellClass('is-zebra-column'), customRender: ({ text }) => renderClickableValue('diffGrossAmt', text) }
           ]
         }
       ]
